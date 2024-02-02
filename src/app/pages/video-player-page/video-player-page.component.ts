e657e41e14396059a04f3f9254c01dee4d8c1e97
hamelin zen <nattapong@theprodigy.biz>
2024-02-02 15:28:13 +0700

    music

--- src/app/pages/video-player-page/video-player-page.component.ts
+++ src/app/pages/video-player-page/video-player-page.component.ts
@@ -15,18 +15,18 @@ export class VideoPlayerPageComponent implements OnInit {
     ngOnInit(): void {
         this.videoSources = [
             {
-                title: 'Big Buck Bunny - 2008',
-                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-bigbuckbunny.jpg',
+                title: 'GTR 34',
+                poster: 'https://s.isanook.com/au/0/ui/17/87599/gtr06.jpg',
                 source: 'https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_stereo.ogg'
             },
             {
-                title: 'Sintel - 2010',
-                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-sintel.jpg',
+                title: 'Mclaren',
+                poster: 'https://hips.hearstapps.com/hmg-prod/images/2014-mclaren-p1-1111-charlie-magee-1531410060.jpg',
                 source: 'http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.1080p.mkv'
             },
             {
-                title: 'Tears of Steel - 2012',
-                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-tearsofsteel.jpg',
+                title: 'Lamborghini',
+                poster: 'https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/2023/09_18_refresh/s/gw_hura_s_02_m.jpg',
                 source: 'http://media.xiph.org/mango/tears_of_steel_1080p.webm'
             }
         ];
